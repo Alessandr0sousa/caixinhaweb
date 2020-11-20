e31d82631165bf71b7960bcc14ee843ff488e8be
Alessandro Medeiros de Sousa <alessandro.sousa@gruposimoes.com.br>
2020-11-20 17:05:20 -0300

    Tela update

--- src/pages/pessoa/index.js
+++ src/pages/pessoa/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 import './style.css';
 
@@ -8,7 +8,6 @@ export default class Pessoa extends Component {
     state = {
         pesaoa: []
     };
-
     componentDidMount() {
         this.loadPessoa();
     }
@@ -16,6 +15,11 @@ export default class Pessoa extends Component {
         const { id } = this.props.match.params;
         const response = await api.get(`/pessoa/${id}`);
         this.setState({ pessoa: response.data });
+    }
+    pagar = async () => {
+        const { id } = this.props.match.params;
+        const response = await api.put(`/pessoa/${id}`);
+        console.log(response);
     }
     render() {
         return (
@@ -27,31 +31,31 @@ export default class Pessoa extends Component {
                         <strong>{pessoa.nome}</strong>
                         <div className="parcelas">
                             <label>P1 - </label>
-                            <span className={pessoa.p1 ? "ok" : "nok"} title='Pracela 1'>{pessoa.p1 ? ok : nok}  </span>
+                            <span className={pessoa.p1 ? "ok" : "nok"} title='Pracela 1' onClick={() => this.pagar(pessoa.p1 ? 1 : 0)}>{pessoa.p1 ? ok : nok}  </span>
                             <label>P2 - </label>
-                            <span className={pessoa.p2 ? "ok" : "nok"} title='Pracela 2'>{pessoa.p2 ? ok : nok}  </span>
+                            <span className={pessoa.p2 ? "ok" : "nok"} title='Pracela 2' onClick={() => this.pagar(pessoa.p2 ? 1 : 0)}>{pessoa.p2 ? ok : nok}  </span>
                             <label>P3 - </label>
-                            <span className={pessoa.p3 ? "ok" : "nok"} title='Pracela 3'>{pessoa.p3 ? ok : nok}  </span>
+                            <span className={pessoa.p3 ? "ok" : "nok"} title='Pracela 3' onClick={() => this.pagar(pessoa.p3 ? 1 : 0)}>{pessoa.p3 ? ok : nok}  </span>
                             <label>P4 - </label>
-                            <span className={pessoa.p4 ? "ok" : "nok"} title='Pracela 4'>{pessoa.p4 ? ok : nok}  </span>
+                            <span className={pessoa.p4 ? "ok" : "nok"} title='Pracela 4' onClick={() => this.pagar(pessoa.p4 ? 1 : 0)}>{pessoa.p4 ? ok : nok}  </span>
                             <label>P5 - </label>
-                            <span className={pessoa.p5 ? "ok" : "nok"} title='Pracela 5'>{pessoa.p5 ? ok : nok}  </span>
+                            <span className={pessoa.p5 ? "ok" : "nok"} title='Pracela 5' onClick={() => this.pagar(pessoa.p5 ? 1 : 0)}>{pessoa.p5 ? ok : nok}  </span>
                             <label>P6 - </label>
-                            <span className={pessoa.p6 ? "ok" : "nok"} title='Pracela 6'>{pessoa.p6 ? ok : nok}  </span>
+                            <span className={pessoa.p6 ? "ok" : "nok"} title='Pracela 6' onClick={() => this.pagar(pessoa.p6 ? 1 : 0)}>{pessoa.p6 ? ok : nok}  </span>
                             <label>P7 - </label>
-                            <span className={pessoa.p7 ? "ok" : "nok"} title='Pracela 7'>{pessoa.p7 ? ok : nok}  </span>
+                            <span className={pessoa.p7 ? "ok" : "nok"} title='Pracela 7' onClick={() => this.pagar(pessoa.p7 ? 1 : 0)}>{pessoa.p7 ? ok : nok}  </span>
                             <label>P8 - </label>
-                            <span className={pessoa.p8 ? "ok" : "nok"} title='Pracela 8'>{pessoa.p8 ? ok : nok}  </span>
+                            <span className={pessoa.p8 ? "ok" : "nok"} title='Pracela 8' onClick={() => this.pagar(pessoa.p8 ? 1 : 0)}>{pessoa.p8 ? ok : nok}  </span>
                             <label>P9 - </label>
-                            <span className={pessoa.p9 ? "ok" : "nok"} title='Pracela 9'>{pessoa.p9 ? ok : nok}  </span>
+                            <span className={pessoa.p9 ? "ok" : "nok"} title='Pracela 9' onClick={() => this.pagar(pessoa.p9 ? 1 : 0)}>{pessoa.p9 ? ok : nok}  </span>
                             <label>P10 - </label>
-                            <span className={pessoa.p10 ? "ok" : "nok"} title='Pracela 10'>{pessoa.p10 ? ok : nok}  </span>
+                            <span className={pessoa.p10 ? "ok" : "nok"} title='Pracela 10' onClick={() => this.pagar(pessoa.p10 ? 1 : 0)}>{pessoa.p10 ? ok : nok}  </span>
                             <label>P11 - </label>
-                            <span className={pessoa.p11 ? "ok" : "nok"} title='Pracela 11'>{pessoa.p11 ? ok : nok}  </span>
+                            <span className={pessoa.p11 ? "ok" : "nok"} title='Pracela 11' onClick={() => this.pagar(pessoa.p11 ? 1 : 0)}>{pessoa.p11 ? ok : nok}  </span>
                             <label>P12 - </label>
-                            <span className={pessoa.p12 ? "ok" : "nok"} title='Pracela 12'>{pessoa.p12 ? ok : nok}  </span>
+                            <span className={pessoa.p12 ? "ok" : "nok"} title='Pracela 12' onClick={() => this.pagar(pessoa.p12 ? 1 : 0)}>{pessoa.p12 ? ok : nok}  </span>
                         </div>
-                        <Link to={`/pessoa/${pessoa.id_pes}`}>Novo Pagamento</Link>
+                        {/* <Link to={`/cota/${pessoa.id_pes}`}>Novo Pagamento</Link> */}
                     </article>)
                 })
                 }
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Main from './pages/main';
 import Pessoa from './pages/pessoa';
+import Cota from './pages/cota';
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Main} />
             <Route path="/pessoa/:id" component={Pessoa} />
+            <Route path="/cota/:id" component={Cota} />
         </Switch>
     </BrowserRouter>
 );
