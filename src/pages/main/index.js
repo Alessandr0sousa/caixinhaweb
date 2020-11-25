3ecece6701c98a48af1f27a2f7f2e6ce44721a32
Alessandr0sousa <alessandro.anali@gmail.com>
2020-11-25 01:56:49 -0300

    comclusão do upadate de cotas

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -7,7 +7,7 @@ import './style.css';
 
 export default class Main extends Component {
     state = {
-        pesaoa: []
+        pessoa: []
     };
 
     componentDidMount() {
--- src/pages/pessoa/index.js
+++ src/pages/pessoa/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import './style.css';
 
 export default class Pessoa extends Component {
     state = {
-        pesaoa: []
+        pessoa: []
     };
     componentDidMount() {
         this.loadPessoa();
@@ -16,29 +16,31 @@ export default class Pessoa extends Component {
         const response = await api.get(`/pessoa/${id}`);
         this.setState({ pessoa: response.data });
     }
-    pagar = async (v) => {
+    pagar = async (parcela) => {
         const { id } = this.props.match.params;
-        const response = await api.patch(`/cota/${id}`, `${v}`);
+        const response = await api.patch(`/cota/${id}`, { parcela });
         console.log(response);
     }
     render() {
         return (
             <div className="pessoa-list" >
+                <Link to="/">Voltar</Link>
                 {this.state.pessoa && this.state.pessoa.map(pessoa => {
                     const ok = String.fromCharCode(10004);
                     const nok = String.fromCharCode(10008);
-                    return (<article key={pessoa.id_cota}>
-                        <strong>{pessoa.nome}</strong>
-                        <div className="parcelas">
-                            {pessoa.parcelas && pessoa.parcelas.map((parcela, index) => (
-                                <div key={index} className="parcelas">
-                                    <label>P{index + 1} - </label>
-                                    <span className={parcela ? "ok" : "nok"} title={`Parcela${index}`} onClick={() => this.pagar( parcela ? `p${index+1}: 0` : `p${index+1}: 1`)}>{parcela ? ok : nok}</span>
-                                </div>
-                            ))}
-                        </div>
-                        {/* <Link to={`/cota/${pessoa.id_pes}`}>Novo Pagamento</Link> */}
-                    </article>)
+                    return (
+                        <article key={pessoa.id_cota}>
+                            <strong>{pessoa.nome}</strong>
+                            <div className="grid-container">
+                                {pessoa.parcelas && pessoa.parcelas.map((parcela, index) => (
+                                    <div key={index} className="parcelas">
+                                        <label>P{index + 1} - </label>
+                                        <span className={parcela ? "ok" : "nok"} title={`Parcela${index}`} onClick={() => this.pagar(index + 1)}>{parcela ? ok : nok}</span>
+                                    </div>
+                                ))}
+                            </div>
+                        </article>
+                    )
                 })
                 }
             </div>
