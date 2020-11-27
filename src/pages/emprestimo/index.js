ad5f13aa2b75568d6b0e6b3aff53474fffba9b5b
Alessandr0sousa <alessandro.anali@gmail.com>
2020-11-27 17:31:21 -0300

    pg

--- src/pages/emprestimo/index.js
+++ src/pages/emprestimo/index.js
@@ -10,7 +10,7 @@ export default class Emprestimo extends Component {
   };
   componentDidMount() {
     this.loadEmprestimo();
-    // this.loadPagamento();
+    this.loadPagamento();
   }
   loadEmprestimo = async () => {
     const { id } = this.props.match.params;
@@ -39,12 +39,13 @@ export default class Emprestimo extends Component {
                   <span className="emprestimo-data">{emprestimo.data}</span>
                 </div>
                 {this.state.pagamento && this.state.pagamento.map(pagamento => {
-                  return(
-                  <div key={emprestimo.id_emp} className="pagamentos">
-                    <span className="pagameto-valor">{"R$ " + emprestimo.valor_juro.toLocaleString('pt-br')}</span>
-                    <span className="pagameto-valor">{"R$ " + emprestimo.valor_quitacao.toLocaleString('pt-br')}</span>
-                  </div>
-                )})}
+                  return (
+                    <div key={emprestimo.id_emp} className="pagamentos">
+                      <span className="pagamento-valor">{"R$ " + emprestimo.valor_juro.toLocaleString('pt-br')}</span>
+                      <span className="pagamento-valor">{"R$ " + emprestimo.valor_quitacao.toLocaleString('pt-br')}</span>
+                    </div>
+                  )
+                })}
               </div>
             </article>);
         })}
