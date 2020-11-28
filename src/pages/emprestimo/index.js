3468d77eecc7e98ef169ce0b39123ef041a79b38
Alessandr0sousa <alessandro.anali@gmail.com>
2020-11-28 12:12:24 -0300

    mais uma

--- src/pages/emprestimo/index.js
+++ src/pages/emprestimo/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 // import { Link } from "react-router-dom";
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
 import './styles.css';
 
 export default class Emprestimo extends Component {
@@ -22,7 +26,7 @@ export default class Emprestimo extends Component {
     const { id } = this.props.match.params;
     const response = await api.get(`/pagamento/${id}`);
     this.setState({ pagamento: response.data });
-    console.log({id});
+    console.log({ id });
   }
   render() {
     return (
@@ -38,11 +42,22 @@ export default class Emprestimo extends Component {
                 <div>
                   <span className="emprestimo-data">{emprestimo.data}</span>
                 </div>
-                {this.state.pagamento && this.state.pagamento.map(pagamento => {
+                {this.state.pagamento && this.state.pagamento.map((pagamento, index) => {
                   return (
-                    <div key={emprestimo.id_pag} className="pagamentos">
-                      <span className="pagamento-valor">{"R$ " + emprestimo.valor_juro.toLocaleString('pt-br')}</span>
-                      <span className="pagamento-valor">{"R$ " + emprestimo.valor_quitacao.toLocaleString('pt-br')}</span>
+                    <div key={index} className="pagamentos">
+                      <Accordion key={index}>
+                        <Card>
+                          <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey={index}>
+                              Click me!
+                          </Accordion.Toggle>
+                          </Card.Header>
+                          <Accordion.Collapse eventKey={index}>
+                            <span className="pagamento-valor">{"R$ " + pagamento.valor_juro.toLocaleString('pt-br')}</span>
+                            <span className="pagamento-valor">{"R$ " + pagamento.valor_quitacao.toLocaleString('pt-br')}</span>
+                          </Accordion.Collapse>
+                        </Card>
+                      </Accordion>
                     </div>
                   )
                 })}
