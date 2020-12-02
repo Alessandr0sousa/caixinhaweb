66c81219da1ba3387df22d3147cad040b1950aac
Alessandr0sousa <alessandro.anali@gmail.com>
2020-12-02 01:57:52 -0300

    main primary

--- src/pages/pagamento/index.js
+++ src/pages/pagamento/index.js
@@ -14,33 +14,57 @@ export default class Pagamento extends Component {
         const { id } = this.props.match.params;
         const response = await api.get(`/pagamento/${id}`);
         this.setState({ pagamento: response.data });
-        console.log(response);
+        console.log(response.data);
+    }
+    mostrar() {
+        const groupElement =  document.querySelector('div#form_pag');
+        if (groupElement.style.display === "none") {
+            groupElement.style.display = "block";
+        } else {
+            groupElement.style.display = "none";
+        }
     }
     render() {
         return (
             <div className="pagamento-info" >
-                {/* <h3>{response.data}</h3> */}
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Data</th>
-                            <th>Juro</th>
-                            <th>Quitação</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.pagamento && this.state.pagamento.map(pagamento => {
-                            return (
-                                <tr key={pagamento.id_pag}>
-                                    <td>{pagamento.data}</td>
-                                    <td>{"R$ " + pagamento.valor_juro.toLocaleString('pt-br')}</td>
-                                    <td>{"R$ " + pagamento.valor_quitacao.toLocaleString('pt-br')}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-                <span className="new" title="Adicionar pagamento">&#10006;</span>
+                <article>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Data</th>
+                                <th>Valor Juros</th>
+                                <th>valor Quitação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.pagamento && this.state.pagamento.map(pagamento => {
+                                return (
+                                    <tr key={pagamento.id_pag}>
+                                        <td>{pagamento.data}</td>
+                                        <td>{"R$ " + pagamento.valor_juro.toLocaleString('pt-br')}</td>
+                                        <td>{"R$ " + pagamento.valor_quitacao.toLocaleString('pt-br')}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                    <div className="click-group">
+                        <span className="new" title="Adicionar pagamento" onClick={() => this.mostrar()}>&#10006;</span>
+                        <label className="click">Click</label>
+                    </div>
+                </article>
+                <div className="toot">
+                    <span>Totais</span>
+                    <span>{this.state.pagamento.reduce(acc, pag) => acc += pag.valor_juro}</span>
+                </div>
+                <div className="form-group" id="form_pag">
+                    <label className="form-control">Juros</label>
+                    <input type="text" className="form-control"/>
+                    <label className="form-control">Quitação</label>
+                    <input type="text" className="form-control"/>
+                    <button className="btn-envio">Enviar</button>
+                </div>
+
             </div >
         );
     }
