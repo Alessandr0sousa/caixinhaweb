e44ed1fa3559a767e29b43c23a883b13bc2daec9
Alessandr0sousa <alessandro.anali@gmail.com>
2020-12-07 17:54:36 -0300

    novo

--- src/pages/emprestimo/index.js
+++ src/pages/emprestimo/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 import { Link } from "react-router-dom";
+import uniqid from 'uniqid';
 import './styles.css';
 
 export default class Emprestimo extends Component {
@@ -23,14 +24,50 @@ export default class Emprestimo extends Component {
     this.setState({ pagamento: response.data });
     console.log(response);
   }
+  salvar = async () => {
+    const { id } = this.props.match.params;
+    const id_emp = uniqid();
+    const pessoa = id;
+    const valor = document.querySelector('input#emprestimo-valor').value;
+    console.log({valor,id_emp, pessoa});
+    if (valor === "") {
+      return alert("Você precisa informar o valor do emprestimo!");
+  } else {
+      await api.post(`/emprestimo`, { valor,id_emp, pessoa });
+      alert("Rempréstimo Realizado com sucesso!");
+      window.location.reload();
+  }
+  }
+  mostrar() {
+    const groupElement = document.querySelector('div#register');
+    if (groupElement.style.display === "none") {
+      groupElement.style.display = "block";
+    } else {
+      groupElement.style.display = "none";
+    }
+  }
   render() {
     return (
       <div className="emprestimo-info" >
+        <div>
+          <div className="add_emprestimo">
+            <h2>Emprestimos </h2>
+            <div className="click-group">
+              <span className="new" title="Novo empréstimo" onClick={() => this.mostrar()}>&#10006;</span>
+              <label className="click">Click</label>
+            </div>
+          </div>
+          <div className="form-group" id="register">
+            <label className="form-control">Valor</label>
+            <input type="text" className="form-control" id="emprestimo-valor" placeholder="R$ 100,00" />
+            <button id="envio" className="btn-success" onClick={() => this.salvar()}>Enviar</button>
+          </div>
+        </div>
         {this.state.emprestimo && this.state.emprestimo.map(emprestimo => {
           return (
             <article key={emprestimo.id_emp}>
               <div className="emprestimos">
-                <label>Valor Inicial</label>
+                <label>Valor emprestado</label>
                 <div>
                   <span className="emprestimo-valor">{"R$ " + emprestimo.valor.toLocaleString('pt-br')}</span>
                 </div>
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 import uniqid from 'uniqid';
+import uniq from 'uniqid';
 import { Link } from "react-router-dom";
 
 import './style.css';
@@ -25,26 +26,30 @@ export default class Main extends Component {
         }
     }
     salvar = async () => {
+        const id_cota = uniq();
         const id_pes = uniqid();
         const nome = await document.getElementById('pessoa-nome').value;
-        const telefone = await document.getElementById('pessoa-email').value;
-        const email = await document.getElementById('pessoa-telefone').value;
+        const telefone = await document.getElementById('pessoa-telefone').value;
+        const email = await document.getElementById('pessoa-email').value;
         const ativo = 1;
-        if (nome === "" || telefone === "" ) {
+        const grupo = 1;
+        const pessoa = id_pes;
+        console.log({ id_cota, pessoa, grupo });
+        if (nome === "" || telefone === "") {
             return alert("Você precisa preencher pelo menos o nome e o telefone, para realizar esta operação!");
         } else {
             await api.post(`/pessoa`, { id_pes, nome, telefone, email, ativo });
-            alert("Pagamento adicionado com sucesso!")
+            await api.post(`/cota`, { id_cota, pessoa, grupo });
+            alert("Cotista adicionado com sucesso!")
             window.location.reload();
         }
     }
     render() {
-        // console.log(uniqid());
         return (
             <div className="pessoa-list" >
                 <div>
                     <div className="add_contato">
-                        <h2>Cotistas da caixinha 2021</h2>
+                        <h2>Cotistas de 2021</h2>
                         <div className="click-group">
                             <span className="new" title="Adicionar pagamento" onClick={() => this.mostrar()}>&#10006;</span>
                             <label className="click">Click</label>
@@ -53,24 +58,26 @@ export default class Main extends Component {
                     <div className="form-group" id="register">
                         <label className="form-control">Nome</label>
                         <input type="text" className="form-control" id="pessoa-nome" placeholder="Nome" />
-                        <label className="form-control">E-mail</label>
-                        <input type="email" className="form-control" id="pessoa-email" placeholder="email@seuemail.com" />
                         <label className="form-control">Telefone</label>
                         <input type="tel" className="form-control" id="pessoa-telefone" placeholder="(xx) xxxxx-xxxx" />
+                        <label className="form-control">E-mail</label>
+                        <input type="email" className="form-control" id="pessoa-email" placeholder="email@seuemail.com" />
                         <button id="envio" className="btn-success" onClick={() => this.salvar()}>Enviar</button>
                     </div>
                 </div>
-                {this.state.pessoa && this.state.pessoa.map(pessoa => {
-                    return (<article key={pessoa.id_pes}>
-                        <strong>{pessoa.nome}</strong>
-                        <div className="parcelas">
-                            <label>Acumulado: </label>
-                            {"R$ " + (pessoa.p1 + pessoa.p2 + pessoa.p3 + pessoa.p4 + pessoa.p5 + pessoa.p6 + pessoa.p7 + pessoa.p8 + pessoa.p9 + pessoa.p10 + pessoa.p11 + pessoa.p12).toLocaleString('pt-br')}
-                        </div>
-                        <Link to={`/pessoa/${pessoa.id_cota}`}>Detalhar</Link>
-                    </article>);
-                })
-                }
+                <div className="listagem">
+                    {this.state.pessoa && this.state.pessoa.map(pessoa => {
+                        return (<article key={pessoa.id_pes}>
+                            <strong>{pessoa.nome}</strong>
+                            <div className="parcelas">
+                                <label>Acumulado: </label>
+                                {"R$ " + (pessoa.p1 + pessoa.p2 + pessoa.p3 + pessoa.p4 + pessoa.p5 + pessoa.p6 + pessoa.p7 + pessoa.p8 + pessoa.p9 + pessoa.p10 + pessoa.p11 + pessoa.p12).toLocaleString('pt-br')}
+                            </div>
+                            <Link to={`/pessoa/${pessoa.id_cota}`}>Detalhar</Link>
+                        </article>);
+                    })
+                    }
+                </div>
             </div>
         );
     }
